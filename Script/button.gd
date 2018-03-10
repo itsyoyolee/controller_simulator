de2a343e04d3960543f3959ab70c489085974798
Scream <scream870102@gmail.com>
2018-03-10 14:00:25 +0800

    controller and button test

--- Script/button.gd
+++ Script/button.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-# class member variables go here, for example:
+# class member variables go here, for example: 
 # var a = 2
 # var b = "textvar"
-export var buttonType = "int"
+export var buttonType = "normal"
 var timer = 0
 var enable = true
 var dir = "" #L R U D
@@ -14,9 +14,10 @@ func _ready():
 	pass
 
 func _process(delta):
-	pressDetect()
+	
 	#是否被人類按下去 開始計時&特效
 	if(enable):
+		pressDetect()
 		timer += 1
 		$Sprite.offset.y = 10
 		$anim.scale *= Vector2(1.1, 1.1)
@@ -30,11 +31,11 @@ func _process(delta):
 
 func pressDetect(): #是否被小精靈推回去
 	if(buttonType == "normal"):
-		if($area.overlaps_body(get_node("../player/body"))):
-			if(Input.is_action_pressed("ui_select")):
+		if($area.overlaps_body(get_node("../../player/body"))):
+			if(Input.is_action_just_pressed("ui_select")):
 				enable = false
 	elif(buttonType == "mushroom"):
-		if($area.overlaps_body(get_node("../player/body"))):
+		if($area.overlaps_body(get_node("../../player/body"))):
 			if(get_node("../player").catch(dir)):
 				enable = false
 	pass
--- Script/player.gd
+++ Script/player.gd
@@ -1,5 +1,5 @@
 extends Node2D
-const SPEED=1.0
+const SPEED=100.0
 
 func _ready():
 	# Called every time the node is added to the scene.
@@ -12,13 +12,13 @@ func _process(delta):
 
 func Move():
 	if(Input.is_action_pressed("ui_left")):
-		move_and_slide(Vector2(-1,0)*SPEED)
+		$body.move_and_slide(Vector2(-1,0)*SPEED)
 	elif(Input.is_action_pressed("ui_right")):
-		move_and_slide(Vector2(1,0)*SPEED)
+		$body.move_and_slide(Vector2(1,0)*SPEED)
 	elif(Input.is_action_pressed("ui_up")):
-		move_and_slide(Vector2(0,-1)*SPEED)
+		$body.move_and_slide(Vector2(0,-1)*SPEED)
 	elif(Input.is_action_pressed("ui_down")):
-		move_and_slide(Vector2(0,1)*SPEED)
+		$body.move_and_slide(Vector2(0,1)*SPEED)
 	pass
 	
 #得到 蘑菇頭的方向 並判斷使用者的輸入 並回傳結果
