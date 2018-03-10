eb2281e0568018bd4105441bc9b55bcb83d33d66
KurokuLarbouno <k850701.lee@gmail.com>
2018-03-10 13:08:40 +0800

    main
    main

--- Script/main.gd
+++ Script/main.gd
@@ -7,26 +7,25 @@ var game_list = []			#遊戲列表
 var playing_game			#正在遊玩的遊戲
 var controller_health = 100	#玩家總血量
 var REACT_TIME = 5			#單一按鍵失敗時間判定
-var Fail_Time = 100			#反應總時間
 var fail_time 			#反應總時間
 var score = 0				#分數(存活時間相關)
 
 func _ready():
-
+	Init_Btn()
+	fail_time = playing_game.endPoint
 	pass
 
 func _process(delta):
-	if (controller_health <= 0):
-		if (fail_time <= 0):#按鍵設定及判定
+	if (controller_health >= 0):
+		if (fail_time >= 0):#按鍵設定及判定
 			#決定隨機點擊
-
 			Btn_Decide(delta)
 			#如果按鈕沒點擊count fail_time
 			Btn_Update(delta)
 			pass
 		else:
-			fail_time = Fail_Time
 			Game_Change()
+			fail_time = playing_game.endPoint
 	else:
 		Game_Over()
 	pass
@@ -34,18 +33,19 @@ func _process(delta):
 func Btn_Decide(delta):
 	if (input_pass <=0):
 		randomize()
-		var input_btn = get_node("playing_game").byn_list[randi()%get_node("playing_game").byn_list.size()]
-		if button_list[input_btn].enable:
+		var input_btn = get_node("playing_game").btn_list[randi()%get_node("playing_game").btn_list.size()]#抓kind
+		if ! button_list[input_btn].enable:
 			button_list[input_btn].EnableButton()
-			input_pass = INPUT_OFFSET + ((randi()%50)/10 - 2.5)	# -2.5~+2.5
+			input_pass = INPUT_OFFSET - ((randi()%29)/10)
 	else: input_pass -= delta
 	pass
 func Btn_Update(delta):
 	for index in button_list:
 		if button_list[index].enable == 1:
-			fail_time -= button_list[index].timer
+			fail_time -= delta
 	pass
 func Game_Change():
+
 	pass
 func Game_Over():
 	pass
